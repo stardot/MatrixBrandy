10d5f807f53b604f23ceffb6ad55e3a204dbf8ea
Michael McConnell <soruk@eridani.co.uk>
2018-08-20 09:22:25 +0100

    Version 1.21.10

--- src/target.h
+++ src/target.h
@@ -25,8 +25,8 @@
 **	Included a Mac OS X target for conditional compilation.
 */
 
-#define BRANDY_VERSION "1.21.9"
-#define BRANDY_DATE    "10 Aug 2018"
+#define BRANDY_VERSION "1.21.10"
+#define BRANDY_DATE    "20 Aug 2018"
 
 #ifndef __target_h
 #define __target_h
