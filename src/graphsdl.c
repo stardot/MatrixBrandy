455bf696067d465aeb68e7d1b21cc265fb73915c
Michael McConnell <soruk@eridani.co.uk>
2018-11-29 10:13:59 +0000

    Bug fix in VDU19 redraw code

--- src/graphsdl.c
+++ src/graphsdl.c
@@ -1211,7 +1211,7 @@ static void vdu_setpalette(void) {
   if (colourdepth <= 256) {
     c = logcol * 3;
     newcol = SDL_MapRGB(sdl_fontbuf->format, palette[c], palette[c+1], palette[c+2]) + (logcol << 24);
-    for (offset=0; offset < (screenheight*screenwidth); offset++) {
+    for (offset=0; offset < (screenheight*screenwidth*xscale); offset++) {
       if ((*((Uint32*)modescreen->pixels + offset) >> 24) == logcol) *((Uint32*)modescreen->pixels + offset) = newcol;
     }
     blit_scaled(0,0,screenwidth-1,screenheight-1);
