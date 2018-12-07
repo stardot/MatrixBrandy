98832d73064e471663711338bd30757b3f8650e4
jgharston <jgh@mdfs.net>
2018-12-07 20:01:32 +0000

    Add files via upload

---
+++ build/mkDJGPP.bat
@@ -0,0 +1,13 @@
+@rem Make Brandy Basic for DOS with DJGPP compiler
+@rem Edit DJGPP and PATH paths to match your installation
+@
+@set DJGPP=C:\Apps\Programming\djgpp\djgpp.env
+@set PATH=C:\Apps\Programming\djgpp\bin;%PATH%
+@
+@cd %0\..
+@make -f makefile.djgpp nodebug
+@mkdir ..\binaries >NUL: 2>NUL:
+@if exist brandy.exe copy brandy.exe ..\binaries\brandyDJP.exe >NUL:
+@if exist brandy del brandy
+@if exist brandy.exe del brandy.exe
+@pause
