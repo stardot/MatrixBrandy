ed36ffb475cd32dde32bd42b909c4aa8bd5ae83a
Michael McConnell <soruk@eridani.co.uk>
2019-09-06 23:09:37 +0100

    TOINT mis-implemented. Fixed.

--- src/miscprocs.c
+++ src/miscprocs.c
@@ -555,5 +555,5 @@ FILE *secure_tmpnam(char *name)
 
 int32 TOINT(float64 fltmp) {
   if ((fltmp > 2147483647.0) || (fltmp < -2147483648.0)) error(ERR_RANGE);
-  return (int32)(floor(fltmp));
+  return (int32)fltmp;
 }
