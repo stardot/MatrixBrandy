725396c06ac1be9b4bc16d986ed6f195fdc4ebe9
Michael McConnell <soruk@eridani.co.uk>
2021-02-14 20:41:43 +0000

    V1.22.10

--- src/target.h
+++ src/target.h
@@ -35,8 +35,8 @@
 #define BRANDY_NAME  "Matrix"
 #define BRANDY_MAJOR "1"
 #define BRANDY_MINOR "22"
-#define BRANDY_PATCHLEVEL "9"
-#define BRANDY_DATE       "06 Jan 2021"
+#define BRANDY_PATCHLEVEL "10"
+#define BRANDY_DATE       "14 Feb 2021"
 // #define BRANDY_PATCHDATE  "JGH191007"
 
 /*
