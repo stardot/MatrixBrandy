580064058f8744b21f7cf5aeaeb0d16f176fb364
Michael McConnell <soruk@eridani.co.uk>
2021-03-28 11:04:05 +0100

    Broke MinGW builds. Oops

--- src/mos_sys.c
+++ src/mos_sys.c
@@ -632,7 +632,9 @@ void mos_sys_ext(size_t swino, size_t inregs[], size_t outregs[], int32 xflag, s
         outregs[4]=(MACTYPE >> 8);
         outregs[5]=LEGACY_OSVERSION;
         outregs[6]=getpid();
+#ifdef TARGET_UNIX
         outregs[7]=getppid();
+#endif
         break;
     case SWI_Brandy_MouseEventExpire:
 #ifdef USE_SDL
