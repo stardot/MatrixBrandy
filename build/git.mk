d777d1b005ca41e0917aef4e15c505c7ea5e0c01
Michael McConnell <soruk@eridani.co.uk>
2022-06-02 23:30:49 +0100

    Remove build dependency on 'rev' as it doesn't exist in Solaris

--- build/git.mk
+++ build/git.mk
@@ -2,7 +2,7 @@ gitcommit=\""$(shell git log --abbrev-commit -1 2>/dev/null| head -1 |cut -d ' '
 ifeq ($(gitcommit),\"""\")
   GITFLAGS =
 else
-  gitbranch=\""$(shell git status | head -1 | rev | cut -d ' ' -f 1 | rev)"\"
+  gitbranch=\""$(shell git status | head -1 | sed 's/\# //' | cut -d ' ' -f 3 )"\"
   gitdate=\""$(shell git log --abbrev-commit -1 | grep 'Date:' | cut -d ' ' -f 4-9)"\"
   GITFLAGS = -DBRANDY_GITCOMMIT=$(gitcommit) -DBRANDY_GITBRANCH=$(gitbranch) -DBRANDY_GITDATE=$(gitdate)
 endif
