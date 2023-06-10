stardot/MatrixBrandy

commit e43842e6f1f4a62f141074cb938b121d7e1f6899

LOCAL with no parameters now is a no-op when cosmetic errors are ignored.

src/mainstate.c
@@ -1433,6 +1433,9 @@ void exec_local(void) {
     check_ateol();
     push_data(basicvars.datacur);
     break;
+  case BASIC_TOKEN_EOL: /* No parameter given - Acorn does nothing */
+  case ':':             /* : character, end of statement */
+    if (!basicvars.runflags.flag_cosmetic) break;
   default:	/* Defining local variables */
     def_locvar();
   }

commit 68693f9f5aed300c3f0b558bd96743e5ca866b9d

LOCAL with no parameters now reports an error in immediate mode or outside an FN/PROC, fixing the regression introduced by commit e43842e

src/mainstate.c
@@ -1435,7 +1435,7 @@ void exec_local(void) {
     break;
   case BASIC_TOKEN_EOL: /* No parameter given - Acorn does nothing */
   case ':':             /* : character, end of statement */
-    if (!basicvars.runflags.flag_cosmetic) break;
+    if (!basicvars.runflags.flag_cosmetic && (basicvars.procstack != NIL)) break;
   default:	/* Defining local variables */
     def_locvar();
   }