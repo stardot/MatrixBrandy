0a3346d8302558cdec36fe6b0e35aa3b8dd0f67e
Michael McConnell <soruk@eridani.co.uk>
2024-04-14 11:34:40 +0100

    Build: Autodetect whether a development or release build

--- build/git.mk
+++ build/git.mk
@@ -1,8 +1,11 @@
 gitcommit=\""$(shell git log -1 --format=%h)"\"
 ifeq ($(gitcommit),\"""\")
-  GITFLAGS =
+  GITFLAGS = -DBRANDY_RELEASE
 else
   gitbranch=\""$(shell git rev-parse --abbrev-ref HEAD)"\"
   gitdate=\""$(shell git log -1 --format=%cd)"\"
   GITFLAGS = -DBRANDY_GITCOMMIT=$(gitcommit) -DBRANDY_GITBRANCH=$(gitbranch) -DBRANDY_GITDATE=$(gitdate)
+  ifeq ($(gitbranch),\""master"\")
+    GITFLAGS += -DBRANDY_RELEASE
+  endif
 endif
